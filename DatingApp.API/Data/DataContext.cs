1d7878ea4967cfaa093508f24c102828be582e53
AviD <avi413@gmail.com>
2020-12-20 17:06:51 +0200

    initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+        {
+        }
+        public DataContext(DbContextOptions<DbContext> options) : base (options){}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            if (!options.IsConfigured)
+            {
+                options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=DatingApp;Trusted_Connection=True;MultipleActiveResultSets=True");
+            }
+        }
+        public DbSet<Value> Values { get; set; }
+    }
+}
